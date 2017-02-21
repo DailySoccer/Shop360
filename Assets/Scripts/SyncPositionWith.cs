f13dbcde6d1e18ad39afc39d4a3a7f1bf7f55bcd
korgan00 <korgan00@hotmail.com>
2017-02-21 12:24:55 +0100

    stereo tests working

---
+++ Assets/Scripts/SyncPositionWith.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SyncPosition : MonoBehaviour {
+
+	public Transform with = null;
+
+	void Awake() {
+		enabled = !Application.isPlaying;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = with.position;
+	}
+}
